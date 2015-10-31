0bd017c0a9aa26902f513c8ff930b125ef5db4f3
Damian Prus <prus.damian@gmail.com>
2015-10-31 13:58:07 +0100

    Fix package config

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import Server from 'socket.io';
 
 export default function startServer(store) {
   const io = new Server().attach(8090);
+  console.log('Server started');
 
   store.subscribe(
     () => io.emit('state', store.getState().toJS())
